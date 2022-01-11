ff3e344bc831fbefd88fe59f85e66db5299ed097
SnowPhoenix0105 <1595763343@qq.com>
2022-01-11 13:54:26 +0800

    Add json2tree.MergeString()

--- internal/json2tree/merge.go
+++ internal/json2tree/merge.go
@@ -3,7 +3,13 @@ package json2tree
 import (
 	"github.com/SnowPhoenix0105/cfgm/internal/tree"
 	"io"
+	"strings"
 )
+
+func MergeString(root *tree.Node, json string, time tree.ModifyTime) error {
+	reader := strings.NewReader(json)
+	return Merge(root, reader, time)
+}
 
 func Merge(root *tree.Node, reader io.RuneReader, time tree.ModifyTime) error {
 	par := parser{
